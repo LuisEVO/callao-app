c36eec2f4833777e98f1ee24a68d304528846ccd
Luis Vilcarromero <luis.evo_8.10@hotmail.com>
2020-10-12 22:48:45 -0500

    message

--- src/utils/twilio.util.ts
+++ src/utils/twilio.util.ts
@@ -6,10 +6,7 @@ const env = yenv();
 export default class {
 
   static send(phone: number, message: string) {
-    console.log(env.TWILIO.SID);
-    console.log(env.TWILIO.TOKEN);
     const client = twilio(env.TWILIO.SID, env.TWILIO.TOKEN);
-
     return client.messages
         .create({
           body: message,
